78c2ada7809456297de77550d065d6db2ca146d7
turnermanxi <cturnerelrey@gmail.com>
2024-09-18 03:34:43 -0500

    fixed footer and made layout into grid for easier css responses

--- SmokeScreenSmells/src/App.jsx
+++ SmokeScreenSmells/src/App.jsx
@@ -11,29 +11,30 @@ function App() {
 
   return (
     <>
-      <nav id="header">
-      <div id="canvas2">
-            <Logoscene />
+      <body>
+        <nav id="header">
+        <div id="canvas2">
+              <Logoscene />
+          </div>
+          <ul>
+            <li>Products</li>
+            <li>Services</li>
+            <li>Gallery</li>
+          </ul>
+        </nav>
+        <div id="canvases">
+          <div id="canvas-container1">
+            <Scene1 />
+            <p id="redSS"> RedPuffer: $9.99 </p>
+            <button id="button1">Add To Cart</button>
+          </div>
+          <div id="canvas-container1">
+            <Scene2 />
+            <p>Rolling Tray: $19.99</p>
+            <button id="button1">Add To Cart</button>
+          </div>
         </div>
-        <ul>
-          <li>Products</li>
-          <li>Services</li>
-          <li>Gallery</li>
-        </ul>
-      </nav>
-
-      <div id="canvases">
-        <div id="canvas-container1">
-          <Scene1 />
-          <p id="redSS"> RedPuffer: $9.99 </p>
-          <button id="button1">Add To Cart</button>
-        </div>
-        <div id="canvas-container1">
-          <Scene2 />
-          <p>Rolling Tray: $19.99</p>
-          <button id="button1">Add To Cart</button>
-        </div>
-      </div>
+      </body>
 
       <div id="copyright">
         <footer>
